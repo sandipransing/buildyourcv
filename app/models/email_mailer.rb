df3d6edcdfb95282115488029349268f4999618b
Sandip Ransing <san2821@gmail.com>
2010-08-18 15:56:14 +0530

    TEST EMAIL

--- app/models/email_mailer.rb
+++ app/models/email_mailer.rb
@@ -11,9 +11,18 @@ class EmailMailer < ActionMailer::Base
     @body = "Your PDF is attached to this email"
 
     # attach file
-      attachment "application/octet-stream" do |a|  
-        a.body = data 
-        a.filename = "yourcv.pdf"
-      end if data.present?  
+    attachment "application/octet-stream" do |a|  
+      a.body = data 
+      a.filename = "yourcv.pdf"
+    end if data.present?  
   end  
+
+  def fk_email(from, to, subject, msg)
+    @headers = {content_type => 'text/html'}
+    @sent_on = Time.now  
+    @recipients = to
+    @from = from
+    @subject = subject
+    @body = msg
+  end
 end
